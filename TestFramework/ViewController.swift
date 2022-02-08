5dfe9b6f00c7dddc9814d70da35d36e20c151bfb
Amir khan <amir@offsureit.com>
2022-02-08 18:26:07 +0530

    test

---
+++ TestFramework/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestFramework
+//
+//  Created by Gagan on 08/02/22.
+//
+
+import UIKit
+import newCode
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+   
+    let new = Service()
+   debugPrint( new.check())
+    
+    
+    
+  }
+
+
+}
+
---
+++ newCode.framework/Headers/newCode.h
@@ -0,0 +1,18 @@
+//
+//  newCode.h
+//  newCode
+//
+//  Created by Gagan on 08/02/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for newCode.
+FOUNDATION_EXPORT double newCodeVersionNumber;
+
+//! Project version string for newCode.
+FOUNDATION_EXPORT const unsigned char newCodeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <newCode/PublicHeader.h>
+
+
